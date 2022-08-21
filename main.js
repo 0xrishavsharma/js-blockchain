69309d978c0112e6338782ac16ddea6d361a9888
Rishav Sharma <rishabhtonic2000@gmail.com>
2022-08-21 16:37:25 +0530

    second

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const SHA256 = require("crypto-js/sha256");
 
 
 class Block{
@@ -11,10 +12,55 @@ class Block{
     }
 
     calculateHash() {
-        
+        return SHA256(this.index + this.previousHash + this.timestamp + this.nonce).toString();
     }
 
-    mineBlock() {
+    mineBlock(difficulty) {
 
     }
 }
+
+class Blockchain{
+    constructor() {
+        this.chain = [this.createGenesis()];
+    }
+
+    createGenesis() {
+        return new Block(0, "21/08/2022", "Genesis Block", "0")
+    }
+
+    latestBlock() {
+        return this.chain[(this.chain.length - 1)]
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.latestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    checkValid() {
+        for (let i = 0; i <= (this.chain.length - 1); i++){
+
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+            
+            if (currentBlock.hash !== currentBlock.calculateHash() ) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false
+            }
+
+            return true;
+        }
+    }
+}
+
+let jsChain = new Blockchain();
+jsChain.addBlock(new Block("12/25/2017", {amount: 5}));
+jsChain.addBlock(new Block("12/26/2017", {amount: 10}));
+
+console.log(JSON.stringify(jsChain, null, 4));
+console.log("Is blockchain valid? " + jsChain.checkValid());
